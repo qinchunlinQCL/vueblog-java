d9c70323bdd6965b66aecab48d5e8641a7909cfd
qinchunlinQCL <a1347229016@163.com>
2021-01-12 16:26:58 +0800

    aa

--- src/main/java/com/qcl/vueblogjava/VueblogJavaApplication.java
+++ src/main/java/com/qcl/vueblogjava/VueblogJavaApplication.java
@@ -2,12 +2,17 @@ package com.qcl.vueblogjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "com.qcl")
-public class VueblogJavaApplication {
+public class VueblogJavaApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(VueblogJavaApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(VueblogJavaApplication.class);
+    }
 }
